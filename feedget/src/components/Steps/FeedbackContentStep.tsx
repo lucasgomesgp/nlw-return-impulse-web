3c6023b63422e16130798355ead10ea0e3ae7dd3
Lucas Gomes <lucasgomesbt@gmail.com>
2022-05-07 02:10:55 -0300

    Projeto web e server em NodeJS

---
+++ feedget/src/components/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,76 @@
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FormEvent, useState } from "react";
+import { api } from "../../lib/api";
+import { CloseButton } from "../CloseButton";
+import { Loading } from "../Loading";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { FeedbackType, feedbackTypes } from "../WidgetForm";
+
+interface FeedBackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
+}
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested,
+  onFeedbackSent,
+}: FeedBackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+
+  async function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+    setIsSendingFeedback(true);
+    await api.post("/feedbacks", {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+    setIsSendingFeedback(false);
+    onFeedbackSent();
+  }
+  return (
+    <>
+      <header>
+        <button type="button" onClick={onFeedbackRestartRequested}>
+          <ArrowLeft
+            weight="bold"
+            className="absolute text-zinc-400 hover:text-zinc-100 w-4 h-4 top-5 left-5"
+          />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+      <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-500 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontecendo..."
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500 disabled:cursor-not-allowed"
+            disabled={comment.length === 0 || isSendingFeedback}
+          >
+            {isSendingFeedback ? <Loading /> : "Enviar feedback"}
+          </button>
+        </footer>
+      </form>
+    </>
+  );
+}
---
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -0,0 +1,49 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn()
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail:  sendMailSpy}
+);
+
+describe("submit feedback", () => {
+  it("should be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "data:image/png;base64test.jpg",
+      })
+    ).resolves.not.toThrow();
+    expect(createFeedbackSpy).toBeCalled();
+    expect(sendMailSpy).toBeCalled();
+  });
+  it("should not be able submit feedback without type", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "example comment",
+        screenshot: "data:image/png;base64test.jpg",
+      })
+    ).rejects.toThrow();
+  });
+  it("should not be able submit feedback without comment", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "IDEA",
+        comment: "",
+        screenshot: "data:image/png;base64test.jpg",
+      })
+    ).rejects.toThrow();
+  });
+  it("should not be able submit feedback without screenshot", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "IDEA",
+        comment: "tá tudo bugado",
+        screenshot: "test.jpg",
+      })
+    ).rejects.toThrow();
+  });
+});
