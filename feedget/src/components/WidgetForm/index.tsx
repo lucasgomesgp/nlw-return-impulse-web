99aa4e8610c16fd44a1ba960e222d86ddd0b4472
Lucas Gomes <lucasgomesbt@gmail.com>
2022-05-09 15:06:14 -0300

    Tema light/dark com suas respectivas cores

--- feedget/src/components/WidgetForm/index.tsx
+++ feedget/src/components/WidgetForm/index.tsx
@@ -42,7 +42,7 @@ export function WidgetForm() {
   }
 
   return (
-    <div className="bg-white dark:bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
+    <div className="bg-white shadow-[#00000026] dark:bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
       {feedbackSent ? (
         <FeedbackSuccessStep
           onFeedbackRestartRequested={handleRestartFeedback}
@@ -60,7 +60,7 @@ export function WidgetForm() {
           )}
         </>
       )}
-      <footer className="text-zinc-500">
+      <footer className="text-zinc-500 dark:text-zinc-100">
         Feito com ♥ pela{" "}
         <a
           className="underline underline-offset-2"
